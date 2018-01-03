29fa569fbdbdcc188b7cc94b92d30e0a37a01f7f
Ed Perkins <ed@edperkins.co.uk>
2018-01-03 15:43:49 +0000

    expect bike to be working and pass

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,12 +2,17 @@ require_relative "../lib/DockingStation.rb"
 
 describe DockingStation do
 
+bike = DockingStation.new.release_bike
+
 it { is_expected.to respond_to(:release_bike) }
 
 it "release bike should return a bike" do
-  DockingStation.new.release_bike.is_a?(Bike)
+  bike.is_a?(Bike)
 end
 
+it "expects bike to be working" do
+ bike.working? == true
+end
 
 
 
