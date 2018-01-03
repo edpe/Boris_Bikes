530103101203ec849ef869c8a942d171c0310291
Tagrand <thomas.a.grand@gmail.com>
2018-01-03 16:41:32 +0000

    add test dock method and pass

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,8 +2,19 @@ require "./lib/Bike.rb"
 
 class DockingStation
 
+def initialize
+ @dock = []
+end
+
+attr_reader :dock
+
 def release_bike
   Bike.new
 end
+
+def dock_bike(bike)
+  @dock << bike
+end
+
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,16 +4,26 @@ describe DockingStation do
 
 bike = DockingStation.new.release_bike
 
+
 it { is_expected.to respond_to(:release_bike) }
 
 it "release bike should return a bike" do
-  bike.is_a?(Bike)
+  expect(bike.is_a?(Bike)).to eq true
 end
 
 it "expects bike to be working" do
- bike.working? == true
+ expect(bike.working?).to eq true
 end
 
+station = DockingStation.new
+test_bike = Bike.new
+it { is_expected.to respond_to(:dock_bike) }
+it { is_expected.to respond_to(:dock) }
+
+it "expects a bike to be docked" do
+  station.dock_bike(test_bike)
+  expect(station.dock.include?(test_bike)).to eq true
+end
 
 
 
